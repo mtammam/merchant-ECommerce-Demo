f35ad075695f469a35764691811ef804ccd8d8d7
MSMDEA SFD <msmeda@outlook.com>
2022-02-26 11:23:34 +0200

    comit

---
+++ assets/js/CustomJs.js
@@ -0,0 +1,17 @@
+
+function  redirectToProduct()
+{
+ 
+var object={
+name:$('#js-itemName').html(),
+img: $('#img-view').attr('src'),
+desc:$('#js-desc').html(),
+price:$('#js-price').html()
+
+}
+
+console.log(object)
+sessionStorage.setItem("myproduct",JSON.stringify(object) );
+window.location.href="productdetailes.html"
+}
+
